18ac2abf16e10be31dd794e05356b15c4616eb0a
Ellyzes John David <ej54321david@gmail.com>
2024-05-14 19:54:09 +0800

    otin

--- src/app/dashboard/admin/userm/page.tsx
+++ src/app/dashboard/admin/userm/page.tsx
@@ -1,50 +1,43 @@
 import React from 'react';
-import styles from "@/app/ui/dashboard/products/userM.module.css";  // Import the specific CSS for Users
+import styles from "@/app/ui/dashboard/products/userM.module.css";
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { db } from '@/lib/db';
-import { FC } from 'react';
+import UserM from '@/app/component/UserM'
 
 
-interface userName{
-    names:{
-        id: string;
-        firstname: string;
 
-    };
-}
 
-async function getName(){
+async function getName() {
     const response = await db.user.findMany({
-        select:{
+        select: {
             id: true,
             firstname: true,
         },
-        
     });
-    return response;
+    return response.map(user => ({
+        ...user,
+        id: user.id.toString(), 
+        firstname: user.firstname || ""
+    }));
 }
 
-const Users: FC<userName> = async ({names}) => {
+export default async function Home(){
     const session = await getServerSession(authOptions);
-    const name = await getName();
-    console.log(name);
-    
+    const names = await getName();
+    console.log(names);
+
+
     if(session?.user){
-       return( <div className={styles.users}>
-            <h2 className={styles.h2}>Users</h2>
-            {name.map((names) => (
-                <div key={names.id} className={styles.user}>
-                    <span className={styles.span}>{names.firstname}</span>
-                    <button className={styles.buttonEdit}>Edit</button>
-                    <button className={styles.buttonDelete}>Delete</button>
-                </div>
-            ))}
-        </div>
-       );
+    return(
+        <div className={styles.users}>
+        <h2 className={styles.h2}>Users</h2>
+        {names.map((name) =>(
+            <UserM key={name.id} name={name}/>
+        ))}
+    </div>
+
+        )
     }
     return <h2 className='text-2xl' style={{ color: 'black' }}>Please Login to see this admin</h2>;
-    
 };
-
-export default Users;
