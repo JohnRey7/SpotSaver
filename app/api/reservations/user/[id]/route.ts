08209b7e3ddec1b2264ec79fac51af3a98565af9
John Rey Cutab <96228393+JohnRey7@users.noreply.github.com>
2024-05-25 00:31:10 +0800

    update reservations api route

--- app/api/reservations/user/[id]/route.ts
+++ app/api/reservations/user/[id]/route.ts
@@ -1,7 +1,7 @@
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "~/lib/db";
-import { Reservation, reservation } from "~/lib/schema";
+import { reservation } from "~/lib/schema";
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
 	try {
@@ -11,65 +11,13 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
 			return NextResponse.json({ message: "Invalid user ID." }, { status: 400 });
 		}
 
-		const returnedReservation = await db.query.reservation.findFirst({
+		const returnedReservations = await db.query.reservation.findMany({
 			where: eq(reservation.userId, id),
 		});
-		if (!returnedReservation) {
-			return NextResponse.json({ message: "Reservation not found." }, { status: 404 });
-		}
 
 		return NextResponse.json({
-			message: `Successfully fetched reservation from user id ${id}`,
-			reservation: returnedReservation,
-		});
-	} catch (error) {
-		console.error(error);
-		return NextResponse.json({ message: "An error occurred." }, { status: 500 });
-	}
-}
-
-export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
-	try {
-		const id = params.id;
-
-		if (!id) {
-			return NextResponse.json({ message: "Invalid user ID." }, { status: 400 });
-		}
-
-		const body = (await request.json()) as Partial<Reservation>;
-
-		const returnedReservation = await db
-			.update(reservation)
-			.set(body)
-			.where(eq(reservation.userId, id))
-			.returning();
-
-		return NextResponse.json({
-			message: `Successfully updated reservation from user id ${id}`,
-			reservation: returnedReservation[0],
-		});
-	} catch (error) {
-		console.error(error);
-		return NextResponse.json({ message: "An error occurred." }, { status: 500 });
-	}
-}
-
-export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
-	try {
-		const id = params.id;
-
-		if (!id) {
-			return NextResponse.json({ message: "Invalid user ID." }, { status: 400 });
-		}
-
-		const returnedReservation = await db
-			.delete(reservation)
-			.where(eq(reservation.userId, id))
-			.returning();
-
-		return NextResponse.json({
-			message: `Successfully deleted reservation from user id ${id}`,
-			reservation: returnedReservation[0],
+			message: `Successfully fetched all reservations from user id ${id}`,
+			reservations: returnedReservations,
 		});
 	} catch (error) {
 		console.error(error);
