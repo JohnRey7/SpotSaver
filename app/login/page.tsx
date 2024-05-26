0df50eaeb95f56a5fce1749940dc4cd8d3bbab70
Ellyzes John David <145997354+ejdavid626@users.noreply.github.com>
2024-05-27 02:48:47 +0800

    Update login page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,12 @@
 export default function LoginPage() {
 	return (
-		<section className="flex h-screen w-full flex-col items-center justify-center overflow-hidden">
-			<p>Login</p>
-			<form action="/api/auth">
-				<button className="btn btn-error">Login with Google</button>
-			</form>
+		<section className="flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-[url('/cit.png')] bg-cover bg-center">
+			<div className="bg-white bg-opacity-80 p-10 rounded-lg w-96 shadow-lg flex flex-col items-center">
+				<p className="text-2xl mb-4">SpotSaver</p>
+				<form action="/api/auth" className="w-full">
+					<button className="btn btn-error w-full">Login with Google</button>
+				</form>
+			</div>
 		</section>
 	);
 }
